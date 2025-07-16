dbdd30fea53c9d062a88a2e5e773bf4bbb1e1335
Jayasurya R <suryasunrise261@gmail.com>
2025-07-16 09:15:08 +0530

    Each OS page created with new UI component and its work on the basis of props for data transfer

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <>
+    <div className='flex flex-col items-center justify-center pl-6 pr-6 bg-[#0a0a0a] pb-4 text-amber-50 h-screen text-9xl '>
+        
+        <p className='text-center '>
+            <p className='font-extrabold'>404</p>
+             Page Not Found</p>
+    </div>
+    </>
+  );
+};
+
+export default NotFound;
---
+++ src/pages/Windows.jsx
@@ -0,0 +1,14 @@
+import { LampDemo } from "../components/ui/LampDemo";
+
+
+function Windows() {
+  return (
+    <div className="App">
+      
+     
+      <LampDemo name="window"/>
+    </div>
+  );
+}
+
+export default Windows;
